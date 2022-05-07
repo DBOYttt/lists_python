60d3b1f555280ad71f931b24afe34749a7962542
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-05-07 09:56:32 +0200

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,14 +1,17 @@
 #basic list
 numbers = [4, 5, 2, 10, 7]
 print(numbers)
+
 #append list
 numbers = [4, 5, 2, 10, 7]
 numbers.append(13) #append method added to list a 'number' or what ever u want to list
 print(numbers)
+
 #insert list
 numbers = [4, 5, 2, 10, 7]
 numbers.insert(2, 13) #insert method added to list what ever u want in 'index' u want
 print(numbers)
+
 #remove list
 numbers = [4, 5, 2, 10, 7]
 numbers.remove(5) #remove method u can delete what u want
