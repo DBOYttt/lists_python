7916fbf733f7c355c7ecd4f6eafc0d4d6ffe9701
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-05-07 09:54:32 +0200

    (add) explane comments

--- lists.py
+++ lists.py
@@ -3,9 +3,9 @@ numbers = [4, 5, 2, 10, 7]
 print(numbers)
 #append list
 numbers = [4, 5, 2, 10, 7]
-numbers.append(13)
+numbers.append(13) #append method added to list a 'number' or what ever u want to list
 print(numbers)
 #insert list
 numbers = [4, 5, 2, 10, 7]
-numbers.insert(2, 13)
+numbers.insert(2, 13) #insert method added to list what ever u want in 'index' u want
 print(numbers)
